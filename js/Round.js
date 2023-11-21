866bac1d9276ae3ef8adc375a9dd8d28e57dc266
jdcanan <149893114+jdcanan@users.noreply.github.com>
2023-11-20 17:01:37 -0800

    Update Round.js

--- js/Round.js
+++ js/Round.js
@@ -16,8 +16,10 @@ class Round {
   generateWheelValue() {
     let wheelVals = [];
     for (var i = 0; i < wheel.spinValues.length; i++) {
-      let randomIndex = Math.floor(Math.random() * data.wheel.length);
-      wheelVals.push(data.wheel[randomIndex]);
+      //let randomIndex = Math.floor(Math.random() * data.wheel.length);
+      //wheelVals.push(data.wheel[randomIndex]);
+      //We don't want a randomly generated wheel, we want the full wheel to be the same every time
+      wheelVals.push(data.wheel[i]);
     }
     return new Wheel(wheelVals);
   }
