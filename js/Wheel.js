94b94db6e41543783e17b1b1174435142f524ac1
jdcanan <149893114+jdcanan@users.noreply.github.com>
2023-11-22 11:46:35 -0800

    Update Wheel.js

--- js/Wheel.js
+++ js/Wheel.js
@@ -11,6 +11,14 @@ class Wheel {
     this.currentValue = this.spinValues[randomIndex];
     domUpdates.updateCurrentSpin(this.currentValue);
   }
+
+  /* If user selects a wheel value manually, this function should look up and return that wheel value.
+     For development purposes, hardcoding the returned value to 25 for now */
+  getManualSpinValue() {
+    this.currentValue = 25;
+    domUpdates.updateCurrentSpin(this.currentValue);
+  }
+  
 }
 
 
