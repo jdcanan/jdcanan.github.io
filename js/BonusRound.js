85f56ce75b799a5752017bb6f079cd42dc50dbf0
jdcanan <149893114+jdcanan@users.noreply.github.com>
2023-12-02 10:19:10 -0800

    Update BonusRound.js

--- js/BonusRound.js
+++ js/BonusRound.js
@@ -30,17 +30,40 @@ class BonusRound extends Round {
         wheelVals.push(data.bonusWheel[i]);
     }
 
-    // Assuming you have an element with the class .mark1
+    //Need to modify placement of wheel labels so they align properly for bonus wheel values
     const mark1Element = document.querySelector('.mark1');
-
-    // Check if the element exists before modifying its style
-    if (mark1Element) {
-        // Update the left value to 30%
-        mark1Element.style.left = '30%';
-    } else {
-        console.warn('.mark1 element not found.');
-    }
-
+    const mark2Element = document.querySelector('.mark2');
+    const mark3Element = document.querySelector('.mark3');
+    const mark4Element = document.querySelector('.mark4');
+    const mark5Element = document.querySelector('.mark5');
+    const mark6Element = document.querySelector('.mark6');
+    const mark7Element = document.querySelector('.mark7');
+    const mark8Element = document.querySelector('.mark8');
+    const mark9Element = document.querySelector('.mark9');
+    const mark10Element = document.querySelector('.mark10');
+    const mark11Element = document.querySelector('.mark11');
+    const mark12Element = document.querySelector('.mark12');
+    const mark13Element = document.querySelector('.mark13');
+    const mark14Element = document.querySelector('.mark14');
+    const mark15Element = document.querySelector('.mark15');
+    const mark16Element = document.querySelector('.mark16');
+    mark1Element.style.top = '5%';
+    mark2Element.style.top = '5%';
+    mark3Element.style.top = '5%';
+    mark4Element.style.top = '5%';
+    mark5Element.style.top = '5%';
+    mark6Element.style.top = '5%';
+    mark7Element.style.top = '5%';
+    mark8Element.style.top = '5%';
+    mark9Element.style.top = '5%';
+    mark10Element.style.top = '5%';
+    mark11Element.style.top = '5%';
+    mark12Element.style.top = '5%';
+    mark13Element.style.top = '5%';
+    mark14Element.style.top = '5%';
+    mark15Element.style.top = '5%';
+    mark16Element.style.top = '5%';
+    
     return new Wheel(wheelVals);
 }
 
