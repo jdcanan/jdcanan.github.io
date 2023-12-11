9acced8312838cb5e04a4bef4bc7694575e73938
jdcanan <149893114+jdcanan@users.noreply.github.com>
2023-12-10 16:32:42 -0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -219,7 +219,49 @@ function newGameHandler(e) {
     $('.spin-button').on('click', game.setUpWheel);
     $('.solve-button').on('click', domUpdates.displaySolvePopup);
     $('.vowel-button').on('click', vowelPurchaseHandler);
-    
+
+    //Need to reset placement of wheel labels so they align properly for regular wheel values
+    const mark1Element = document.querySelector('.mark1');
+    const mark2Element = document.querySelector('.mark2');
+    const mark3Element = document.querySelector('.mark3');
+    const mark4Element = document.querySelector('.mark4');
+    const mark5Element = document.querySelector('.mark5');
+    const mark6Element = document.querySelector('.mark6');
+    const mark7Element = document.querySelector('.mark7');
+    const mark8Element = document.querySelector('.mark8');
+    const mark9Element = document.querySelector('.mark9');
+    const mark10Element = document.querySelector('.mark10');
+    const mark11Element = document.querySelector('.mark11');
+    const mark12Element = document.querySelector('.mark12');
+    const mark13Element = document.querySelector('.mark13');
+    const mark14Element = document.querySelector('.mark14');
+    const mark15Element = document.querySelector('.mark15');
+    const mark16Element = document.querySelector('.mark16');
+    const option5Element = document.querySelector('.option5');
+    const option14Element = document.querySelector('.option14');    
+    mark1Element.style.top = '8%';
+    mark2Element.style.top = '10%';
+    mark3Element.style.top = '10%';
+    mark4Element.style.top = '8%';
+    mark5Element.style.top = '8%';
+    mark5Element.style.fontSize = '1.2rem';
+    mark5Element.style.left = '30.5%';
+    mark5Element.style.color = 'white';
+    mark6Element.style.top = '10%';
+    mark7Element.style.top = '10%';
+    mark8Element.style.top = '10%';
+    mark9Element.style.top = '8%';
+    mark10Element.style.top = '10%';
+    mark11Element.style.top = '10%';
+    mark12Element.style.top = '8%';
+    mark13Element.style.top = '10%';
+    mark14Element.style.top = '6%';
+    mark14Element.style.left = '30.5%';
+    mark15Element.style.top = '10%';
+    mark16Element.style.top = '10%';
+    option5Element.style.background = '#000000';
+    option14Element.style.background = '#ffffff';
+
     game.quitGame();
   }
 }
