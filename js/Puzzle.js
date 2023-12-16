0439a0e9cb561735833a905cdcb3f797537d5de4
jdcanan <149893114+jdcanan@users.noreply.github.com>
2023-12-16 09:08:47 -0800

    Update Puzzle.js

--- js/Puzzle.js
+++ js/Puzzle.js
@@ -10,7 +10,7 @@ class Puzzle {
   }
 
   populateBoard() {
-    console.log("Puzzle: ", this.curentPuzzle.correct_answer);
+    console.log("Puzzle: ", this.currentPuzzle.correct_answer);
     let puzzleArray = this.currentPuzzle.correct_answer.split('');
     domUpdates.populatePuzzleSquares(puzzleArray);
   }
