0d50d641bdfe1daaa055b66fd3f4709dd64155f2
jdcanan <149893114+jdcanan@users.noreply.github.com>
2023-12-20 11:05:00 -0800

    Update Puzzle.js

--- js/Puzzle.js
+++ js/Puzzle.js
@@ -8,7 +8,7 @@ class Puzzle {
     this.numberCorrect = 0;
     this.completed = false;
     try {
-      const puzzleLines = this.generatePuzzleGridLines(puzzleAnswer);
+      const puzzleLines = this.generatePuzzleGridLines(currentPuzzle);
       console.log(puzzleLines);
     } catch (error) {
       console.error(error.message);
