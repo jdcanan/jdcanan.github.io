223cd0a73451bfd0431786368998da29b72bdf30
jdcanan <149893114+jdcanan@users.noreply.github.com>
2024-01-02 21:19:06 -0800

    Update Puzzle.js

--- js/Puzzle.js
+++ js/Puzzle.js
@@ -148,7 +148,7 @@ generatePuzzleGridLines(puzzleAnswer) {
 
     if (remainingVowels.length === 0) {
       //console.log('No more vowels');
-      domUpdates.yellCurrentSpin('NO MORE\nVOWELS');
+      domUpdates.yellCurrentSpin('NO MORE<br>VOWELS');
       setTimeout(domUpdates.yellCurrentSpin, 2000);
       $('.vowel-button').prop('disabled', true);
     }
